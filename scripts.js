53d0ee95afbdf3fdbd6bcdfaddcd3aeecd0343c6
dinaramanausaite <dina.ramanauskaite@gmail.com>
2024-09-16 19:00:55 +0200

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const burger = document.querySelector(".burger");
-    const navLinks = document.querySelector(".nav-links");
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".nav-links");
   
-    burger.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-    });
-  
-    document.querySelectorAll(".nav-links a").forEach((link) => {
-      link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
-      });
-    });
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navLinks.classList.toggle("active");
   });
+});
 
   document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
